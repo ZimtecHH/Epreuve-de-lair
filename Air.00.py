f4743aa278d3dbaa46aabd9501a0474d283ebcfc
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-11-05 14:59:10 +0100

    Add files via upload

--- Air.00.py
+++ Air.00.py
@@ -2,10 +2,12 @@
 # découpe une chaîne de caractères en tableau
 # (séparateurs : espaces, tabulations, retours à la ligne).
 
-def Split():
-  argument =  input("Rentrez argument : ")
-  argument = argument.split(" ")
-  argument = print(argument)
-  return argument
+def Split(string_séparateur):
+  string_à_couper =  input("Rentrez argument : ")
+  string_à_couper = string_à_couper.split(f"{string_séparateur}")
+  for coupe in string_à_couper :
+   coupe = print(coupe)
+  return coupe
 
-Split()
+
+Split(" ")
