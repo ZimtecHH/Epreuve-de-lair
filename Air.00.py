f0b8173c252bb277745150050a75027b6696b55c
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-11-05 15:16:57 +0100

    Add files via upload

--- Air.00.py
+++ Air.00.py
@@ -2,12 +2,28 @@
 # découpe une chaîne de caractères en tableau
 # (séparateurs : espaces, tabulations, retours à la ligne).
 
-def Split(string_séparateur):
+
+
+def Split():
+  texte_separateur = "Le choix du séparateur \n espace = espace \n tabulations = tab \n retour à la ligne = rl \n"
+
   string_à_couper =  input("Rentrez argument : ")
+  string_séparateur = input(texte_separateur)
+
+  if string_séparateur == "espace" :
+    string_séparateur = " "
+  if string_séparateur == "tab":
+    string_séparateur = " "
+  if string_séparateur == "rl":
+    string_séparateur = "\n"
+  else: print("erreur de séparateur")
+
   string_à_couper = string_à_couper.split(f"{string_séparateur}")
+
   for coupe in string_à_couper :
    coupe = print(coupe)
+
   return coupe
 
 
-Split(" ")
+Split()
